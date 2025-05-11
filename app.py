4282b96569a0e5823d3365e7d9bb08018094d33d
JinNguyen108 <gita.jsin@gmail.com>
2025-05-11 19:51:28 +0700

    Update app.py
    J

--- app.py
+++ app.py
@@ -5,18 +5,11 @@ app = Flask(__name__)
 
 @app.route('/tv-alert', methods=['POST'])
 def tv_alert():
-    try:
-        data = request.get_data(as_text=True)  # nhận text thuần
-        with open("signals.txt", "w") as f:
-            f.write(data.strip())  # Ghi vào file, bỏ khoảng trắng
-        return "Signal received", 200
-    except Exception as e:
-        return str(e), 500
+    data = request.get_data(as_text=True)
+    with open("signals.txt", "w") as f:
+        f.write(data.strip())
+    return "OK", 200
 
 @app.route('/signals.txt', methods=['GET'])
-def get_signal():
+def get_signals():
     return send_file("signals.txt", mimetype='text/plain')
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
